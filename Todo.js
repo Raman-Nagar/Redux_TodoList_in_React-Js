0413783f36bc762f29cb998d032dfa8070d03a84
Raman Nagar <115226318+Raman-Nagar@users.noreply.github.com>
2022-11-24 16:55:51 +0530

    Update Todo.js

--- Todo.js
+++ Todo.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo, deleteTodo, removeTodo } from "../actions";
+import { addTodo, deleteTodo, removeTodo } from "../actions/indexAction";
 
 function Todo() {
     const [inpData, setInpData] = useState();
