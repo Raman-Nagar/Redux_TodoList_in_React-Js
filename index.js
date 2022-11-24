d05ab13c0c0b8bb91182e23288263532370fbd69
Raman Nagar <115226318+Raman-Nagar@users.noreply.github.com>
2022-11-24 16:47:21 +0530

    this file in src folder

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-import { combineReducers } from "redux";
-import TodoReducer from "./TodoReducer";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import Store from './Store';
 
-const rootReducer = combineReducers({
-    TodoReducer
-})
-export default rootReducer;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+// import "./App.css";
+// import { Provider } from "react-redux";
+// import store from "./store";
+
+// function App() {
+//   return (
+//     <Provider store={store}>
+//       <div>App Component</div>
+//     </Provider>
+//   );
+// }
+
+// export default App;
